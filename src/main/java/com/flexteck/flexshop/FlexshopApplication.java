c8f7977279213524cdacedbe11bd30e864128951
flexteck <flexteckse@gmail.com>
2026-07-10 15:17:55 +0100

    Initial Spring boot Project Setup

---
+++ src/main/java/com/flexteck/flexshop/FlexshopApplication.java
@@ -0,0 +1,13 @@
+package com.flexteck.flexshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlexshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flexteck/flexshop/FlexshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flexteck.flexshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
