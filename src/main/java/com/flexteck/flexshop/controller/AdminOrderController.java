ee2bf73356dc95559857d2c1a65e2af30586feb3
flexteck <flexteckse@gmail.com>
2026-09-25 22:36:03 +0100

    fix: use Spring MVC @RequestBody in AdminOrderController

--- src/main/java/com/flexteck/flexshop/controller/AdminOrderController.java
+++ src/main/java/com/flexteck/flexshop/controller/AdminOrderController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.flexteck.flexshop.dto.response.OrderResponse;
@@ -14,7 +15,6 @@ import com.flexteck.flexshop.dto.request.UpdateOrderStatusRequest;
 import com.flexteck.flexshop.service.AdminOrderService;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
